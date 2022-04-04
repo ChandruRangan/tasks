b2d0f1b006e93a8b821cd00275118696eec6b668
agiratech-santhoshk <102363212+agiratech-santhoshk@users.noreply.github.com>
2022-04-04 10:43:26 +0530

    Delete titlecase.js

--- titlecase.js
+++
@@ -1,8 +0,0 @@
-function titlecase(txt){
-    txt = txt.toLowerCase().split(' ');
-    for (var i = 0;i < txt.length; i++) {
-        txt[i] = txt[i].charAt(0).toUpperCase()+ txt[i].slice(1);
-    }
-    return txt.join(' ');
-}
-console.log(titlecase("THIS IS THE TITLE"));
