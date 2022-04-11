28bbe5948ce18b62cc4dde7fed8774165e5ac547
Muthu Sankar <102363172+agiratech-muthu@users.noreply.github.com>
2022-04-11 18:11:28 +0530

    Smallest number task

---
+++ muthu/smallest.js
@@ -0,0 +1,19 @@
+let arr=[1, 10, 3, 11, 6, 15];
+let temp=1;
+function smallest()
+{
+	for(i=0;i<arr.length;i++)
+	{
+      temp=temp+arr[i];
+	  return temp;
+	}
+	console.log(temp)
+}
+smallest();
+
+
+
+
+
+
+
