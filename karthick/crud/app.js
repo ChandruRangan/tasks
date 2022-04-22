72936bb8925d2518267df722854e84bdff52ab33
karthick <karthick.b@agiratech.com>
2022-04-22 19:11:07 +0530

    crud update query working

--- karthick/crud/app.js
+++ karthick/crud/app.js
@@ -55,18 +55,35 @@ app.get("/display", (req, res) => {
       "p.product_name",
       "p.price",
       "c.category_name",
-      "p.created_at"
-    )
+      "p.created_at",
+      "p.updated_at",
+    ).orderBy('p.product_id','asc')
     .then((data) => {
       res.render("display", { product: data });
     })
     .catch((err) => {
-      res.json({ message: err });
+      res.status(400).json({ message: err });
     });
 });
 
-app.get("/update", (req, res) => {
-  res.render("update");
+app.get('/update',(req,res)=>{
+  db("products as p")
+  .join("categories as c", "p.category_id", "c.category_id")
+  .select(
+    "p.product_name",
+    "p.price",
+    "c.category_name",
+    "p.category_id",
+    )
+    .then((data) => {
+        var id=req.query.id;
+        id=Number(id)-1;
+        console.log(id);
+        res.render("update", { product: data,id:id });
+      })
+      .catch((err) => {
+        res.json({ message: err });
+      });
 });
 
 app.post("/search", (req, res) => {
@@ -78,13 +95,14 @@ app.post("/search", (req, res) => {
 
     db("products as p")
     .join("categories as c", "p.category_id", "c.category_id")
-    .where("c.category_name", input)
+    .where("c.category_name",input)
     .select(
       "p.product_id",
       "p.product_name",
       "p.price",
       "c.category_name",
-      "p.created_at"
+      "p.created_at",
+      "p.updated_at",
       )
       .then((data) => {
         res.render("display", { product: data });
@@ -95,6 +113,27 @@ app.post("/search", (req, res) => {
     }
   });
 
+app.post('/updatedata',(req,res)=>{
+  let pid=req.query.id;
+  pid=Number(pid)+1;
+  console.log(pid);
+  const pname = req.body.pname;
+  const catid = req.body.catid;
+  const price = req.body.price;
+  db('products')
+  .where('product_id', '=',pid)
+  .update({
+    product_name: pname,
+    price: price,
+    category_id: catid,
+    updated_at: Date(Date.now())    
+  }).then(()=>{
+    res.redirect('/display');
+  }).catch(e=>{
+    res.status(400).json(e);
+  })
+});
+  
 app.listen(port, () => {
   console.log(`Running Server is http://localhost:${port}`);
 });
