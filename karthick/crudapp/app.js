40724a92d18745e4c08f9f99340d199edeee2fc8
karthick <karthick.b@agiratech.com>
2022-04-24 01:41:08 +0530

    my new crudapp

---
+++ karthick/crudapp/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const bodyparser=require('body-parser');
+app.use(bodyparser.urlencoded({extended:true}));
+const {db} = require("./routes/dbconfig");
+const {category}=require('./routes/category');
+const {products}=require('./routes/products');
+const {deletefn}=require('./routes/delete');
+const {insertfn}=require('./routes/insert');
+const {update}=require('./routes/update');
+const {updatedata}=require('./routes/update');
+const {pidasc}=require('./routes/sort');
+const {pnasc}=require('./routes/sort');
+const {priceasc}=require('./routes/sort');
+const {catasc}=require('./routes/sort');
+const {find}=require('./routes/find');
+app.set("view engine", "ejs");
+
+app.get("/",products);
+app.get('/insert',category);
+app.post('/inserted',insertfn);
+app.get('/update',update);
+app.post('/updatedata',updatedata);
+app.get('/delete',deletefn);
+app.post('/find',find);
+app.get('/pidasc',pidasc);
+app.get('/pnasc',pnasc);
+app.get('/priceasc',priceasc);
+app.get('/catasc',catasc);
+app.listen(5000);
+
---
+++ karthick/crudapp/routes/update.js
@@ -0,0 +1,39 @@
+const { db } = require("./dbconfig");
+
+exports.update = async (req, res) => {
+  const id = parseInt(req.query.id);
+  await db
+    .select("*")
+    .from("products")
+    .where("product_id", id)
+    .then((data) => {
+        db.select("*")
+      .from("categories")
+      .orderBy("category_name", "asc").then((cat)=>{
+          res.render("update", { data: data, id: id,cat:cat });
+        })
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+
+exports.updatedata=(async(req,res)=>{
+    let pid=parseInt(req.query.id);
+    console.log(pid);
+    const pname = req.body.pname;
+    const catid = req.body.catid;
+    const price = req.body.price;
+    db('products')
+    .where('product_id', '=',pid)
+    .update({
+      product_name: pname,
+      price: price,
+      category_id: catid,
+      updated_at: Date(Date.now())    
+    }).then(()=>{
+      res.redirect('/');
+    }).catch(e=>{
+      res.status(400).json(e);
+    })
+  });
