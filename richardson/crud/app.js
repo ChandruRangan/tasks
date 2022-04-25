d6c8594257d328c5cd87e5ba9f028aed40beafc7
agira-richardson <richardson.s@agiratech.com>
2022-04-25 10:09:54 +0530

    finished crud task

--- richardson/crud/app.js
+++ richardson/crud/app.js
@@ -41,8 +41,8 @@ app.get("/view",(req,res)=>{
             "p.product_name",
             "c.category_name",
             "p.price",
-            "p.created_at"
-            )
+            "p.created_at",
+            "p.updated_at")
             .then((data)=>{  
                 res.render("view",{data: data});
                })
@@ -62,7 +62,8 @@ app.get("/view",(req,res)=>{
              "p.product_name",
              "c.category_name",
              "p.price",
-             "p.created_at")
+             "p.created_at",
+             "p.updated_at")
              .then((data)=>{
                  
 
@@ -72,5 +73,79 @@ app.get("/view",(req,res)=>{
              .catch(err => res.status(400).json({message:err}));
    }
 });
+
+app.get("/update"  ,(req, res) => {
+    const id = parseInt(req.query.id);
+     db
+      .select("*")
+      .from("products")
+      .where("products_id", id)
+      .then((data) => {
+          db.select("*")
+        .from("category")
+        .orderBy("category_name", "asc").then((c)=>{
+            res.render("update", { data: data, id: id,c:c });
+          })
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+
+app.post("/updatedata",(req,res)=>{
+    let pid=parseInt(req.query.id);
+    console.log(pid);
+    const product = req.body.product;
+    const category = req.body.category;
+    const price = req.body.price;
+    db('products')
+    .where('products_id', '=',pid)
+    .update({
+      product_name: product,
+      price: price,
+      category_id: category,
+      updated_at: Date(Date.now())    
+    }).then(()=>{
+      res.redirect('/view');
+    }).catch(e=>{
+      res.status(400).json(e);
+    })
+  });
+app.get("/casc", (req, res) => {
+    const { c } = req.query;
+    db("products as p")
+      .join("category as c", "p.category_id", "c.category_id")
+      .select(
+        "p.products_id",
+        "p.product_name",
+        "c.category_name",
+        "p.price",
+        "p.created_at",
+        "p.updated_at"
+      )
+      .orderBy(c)
+      .then((data) => {
+        db.select("*")
+        .from("category")
+        .orderBy("category_name", "asc").then((c)=>{
+        res.render("view", { data: data,c:c});
+      })
+      })
+      .catch((err) => {
+        res.status(400).json({ message: err });
+      });
+  });
+app.get("/delete",(req, res) => {
+    const  id  = parseInt(req.query.id);
+    console.log(id);
+    db("products")
+      .where("products_id", id)
+      .del()
+      .then(() => {
+          res.redirect('view');
+      }).catch(err=>{
+          res.status(400).json(err);
+      });
+  });
 
   app.listen(5000);
