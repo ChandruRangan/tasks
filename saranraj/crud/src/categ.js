56eeac981e87bc737b8718960431d4665226de95
agiratech-saranrajg <saranraj.g@agiratech.com>
2022-04-27 13:06:51 +0530

    Completed CRUD

--- saranraj/crud/src/categ.js
+++
@@ -1,2 +0,0 @@
-const { db }=require('./config/database.js');
-db('categories').select('category_name').where('category_id',5).then((response)=>{console.log(response)});
--- saranraj/jokes/count.js
+++ saranraj/jokes/count.js
@@ -1,13 +1,4 @@
-let k=12326000055745000000;
-let l=k.toString();
-console.log(l)
-let count=0;
-for(let i=k.length;i>0;i--){
-    if(k[i]=='0'){
-        count=count+1;
-    }
-    else{
-        break;
-    }
-}
-console.log(count);
+const axios=require('axios');
+axios.get("https://v2.jokeapi.dev/joke/Programming?type=single").then((res)=>{
+    console.log(res);
+})
