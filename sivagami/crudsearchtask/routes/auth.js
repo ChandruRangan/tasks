5437031a84d176ea42457e7647a0eaeb050a7bbb
agiratech-sivagami <sivagami.p@agiratech.com>
2022-05-14 23:06:38 +0530

    accesstoken

---
+++ sivagami/crudsearchtask/routes/auth.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app=express();
+const router = require('express').Router();
+module.exports=router;
+const jwt = require("jsonwebtoken");
+
+
+
+
+
+
+
+
+
+const config = process.env;
+
+const verifyToken = (req, res, next) => {
+  const token =
+    req.body.token || req.query.token || req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(403).send("A token is required for authentication");
+  }
+  try {
+    const decoded = jwt.verify(token, config.TOKEN_KEY);
+    req.user = decoded;
+  } catch (err) {
+    return res.status(401).send("Invalid Token");
+  }
+  return next();
+};
+
+// module.exports = verifyToken;
